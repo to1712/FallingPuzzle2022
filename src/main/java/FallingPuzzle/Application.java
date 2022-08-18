d12ed1180c8af2fa309395e0a25cd0a7a5206921
ITprofile00 <antoniofilippelli0@gmail.com>
2022-08-18 19:36:54 +0200

    ciao

---
+++ src/main/java/FallingPuzzle/Application.java
@@ -0,0 +1,12 @@
+package FallingPuzzle;
+
+import FallingPuzzle.Application;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Application  {
+	
+
+
+}
---
+++ src/main/java/FallingPuzzle/Main.java
@@ -0,0 +1,10 @@
+package FallingPuzzle;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
+
