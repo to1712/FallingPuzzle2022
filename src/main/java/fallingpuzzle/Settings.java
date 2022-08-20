b76bfaa0a63dff844ec7d7c7b13ad0dc1f4f9421
ITprofile00 <antoniofilippelli0@gmail.com>
2022-08-20 17:01:39 +0200

    ciao

---
+++ src/main/java/fallingpuzzle/Settings.java
@@ -0,0 +1,21 @@
+package fallingpuzzle;
+
+public class Settings {
+	public final static int WINDOW_X_SIZE = 900;
+	public final static int WINDOW_Y_SIZE = 700;
+	
+//ciao
+}
+
+
+/*
+ *public final static int WINDOW_SIZE_IN_PIXEL = 800;
+	public final static int BLOCK_SIZE_IN_PIXEL = 40;
+	public final static int CELL_SIZE = WINDOW_SIZE_IN_PIXEL/BLOCK_SIZE_IN_PIXEL;
+	
+	public final static int MOVE_RIGHT = 0;
+	public final static int MOVE_LEFT = 1;
+	public final static int MOVE_UP = 2;
+	public final static int MOVE_DOWN = 3; 
+ * */
+ 
