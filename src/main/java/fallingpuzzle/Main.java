4b7ef3db83217489746a38aa0da9e1dec8980d83
to1712 <toto.orrico@gmail.com>
2022-08-22 12:11:35 +0200

    delete

--- src/main/java/fallingpuzzle/Main.java
+++
@@ -1,8 +0,0 @@
-package fallingpuzzle;
-
-public class Main {
-	public static void main(String[] args) {
-		FPapplication.main(args);
-	}
-}
-
--- src/main/java/fallingpuzzle/model/Griglia.java
+++
@@ -1,5 +0,0 @@
-package fallingpuzzle.model;
-
-public class Griglia {
-	
-}
