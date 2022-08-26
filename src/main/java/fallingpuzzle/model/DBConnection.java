37ceace86cf488abd677d89d44c45114511e6d5d
ITprofile00 <antoniofilippelli0@gmail.com>
2022-08-26 10:27:23 +0200

    committino2

---
+++ src/main/java/fallingpuzzle/model/DBConnection.java
@@ -0,0 +1,23 @@
+package fallingpuzzle.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	public static void main(String[] args) {
+		String url = "jdbc:sqlite:FallingPuzzle'sDB.db";
+		Connection con;
+		try {
+			con = DriverManager.getConnection(url);
+			if(con!=null && !con.isClosed()) {
+				System.out.println("Connected succesfully");
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
